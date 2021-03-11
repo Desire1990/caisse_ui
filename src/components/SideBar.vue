3ef7b38e4f6f95105221dcb4ae78eb59aba03696
GBstallman <nduwimanaguymich@gmail.com>
2021-03-11 18:39:12 +0200

    some changes on sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="nav-side-menu">
     <div class="menu-list">
-         
             <div>
-             <h2>UBUBIKO</h2>
+                <a href="#"> <h2>UBUBIKO</h2></a>
            </div>    
         
         <li>
-          <router-link to="/"><div class="icons"><i class="fa fa-tachometer-alt"></i></div>  Dashboard</router-link>
+          <router-link to="/"><div class="icons"><i class="fa fa-tachometer" aria-hidden="true"></i>
+</div>  Dashboard</router-link>
         </li>
          <li>
-          <router-link to="/moncompte"> <div class="icons"><i class="fa fa-capsules"></i> </div> Mon Compte </router-link>
+          <router-link to="/moncompte"><div class="icons"><i class="fa fa-usd" aria-hidden="true"></i>
+ </div> Mon Compte </router-link>
         </li>
          <li>
-           <router-link to="/membre"><div class="icons"><i class="fa fa-dollar-sign"></i></div>Membre </router-link>
+           <router-link to="/membre"><div class="icons"><i class="fa fa-users" aria-hidden="true"></i>
+</div>Membre </router-link>
         </li>
          <li>
-           <router-link to="/aidesocial"> <div class="icons"><i class="fa fa-cart-arrow-down"></i></div>  Aide Social </router-link>
+           <router-link to="/aidesocial"> <div class="icons"><i class="fa fa-handshake-o" aria-hidden="true"></i>
+</div>  Aide Social </router-link>
         </li>
              
     </div>
@@ -36,30 +39,29 @@ export default {
 </script>
 
 <style scoped>
-.icons{
+h2{
+    font-family:  "Gill Sans Extrabold", sans-serif;
+    font-weight: bold;
+    color: #e1ffff;
+    text-align: center;
   
+}
+.icons{
+    align-content: flex-start;
+    position: sticky;
 }
 .nav-side-menu {
   overflow: auto;
   font-family: verdana;
   font-weight: 200;
-  background-color: #2e353d;
+  background-color: #007bff;
   position: fixed;
   top: 0px;
   width: 300px;
   height: 100%;
   color: #e1ffff;
 }
-.nav-side-menu .brand {
-  background-color: #23282e;
-  line-height: 50px;
-  display: block;
-  text-align: center;
-  font-size: 14px;
-}
-.nav-side-menu .toggle-btn {
-  display: none;
-}
+
 .nav-side-menu ul,
 .nav-side-menu li {
   list-style: none;
@@ -68,51 +70,7 @@ export default {
   line-height: 35px;
   cursor: pointer;
 }
-.nav-side-menu ul :not(collapsed) .arrow:before,
-.nav-side-menu li :not(collapsed) .arrow:before {
-  font-family: FontAwesome;
-  content: "\f078";
-  display: inline-block;
-  padding-left: 10px;
-  padding-right: 10px;
-  vertical-align: middle;
 
-}
-.nav-side-menu ul .active,
-.nav-side-menu li .active {
-  border-left: 3px solid #d19b3d;
-  background-color: #4f5b69;
-}
-.nav-side-menu ul .sub-menu li.active,
-.nav-side-menu li .sub-menu li.active {
-  color: #d19b3d;
-}
-
-.nav-side-menu ul .sub-menu li.active a,
-.nav-side-menu li .sub-menu li.active a {
-  color: #d19b3d;
-}
-.nav-side-menu ul .sub-menu li,
-.nav-side-menu li .sub-menu li {
-  background-color: #181c20;
-  border: none;
-  line-height: 28px;
-  border-bottom: 1px solid #23282e;
-  margin-left: 0px;
-}
-.nav-side-menu ul .sub-menu li:hover,
-.nav-side-menu li .sub-menu li:hover {
-  background-color: #020203;
-}
-.nav-side-menu ul .sub-menu li:before,
-.nav-side-menu li .sub-menu li:before {
-  font-family: Fontawesome;
-  content: "\f105";
-  display: inline-block;
-  padding-left: 20px;
-  padding-right: 10px;
-  vertical-align: middle;
-}
 .nav-side-menu li {
   padding: auto;
   font-size: 20px;
@@ -124,117 +82,31 @@ export default {
   text-decoration: none;
   color: #e1ffff;
 }
-.nav-side-menu li a i {
+.nav-side-menu li  i {
   padding-left: 10px;
   width: 20px;
   padding-right: 20px;
 }
 .nav-side-menu li:hover {
-  border-left: 3px solid #d19b3d;
-  background-color: #4f5b69;
+  border-radius: 1px solid #34cfdd;
+  background-color: #4b7eb4;
   -webkit-transition: all 1s ease;
   -moz-transition: all 1s ease;
   -o-transition: all 1s ease;
   -ms-transition: all 1s ease;
   transition: all 1s ease;
-}
-@media (max-width: 767px) {
-  .nav-side-menu {
-    position: relative;
-    width: 100%;
-    margin-bottom: 10px;
-  }
-  .nav-side-menu .toggle-btn {
-    display: block;
-    cursor: pointer;
-    position: absolute;
-    right: 10px;
-    top: 10px;
-    z-index: 10 !important;
-    padding: 3px;
-    background-color: #ffffff;
-    color: #000;
-    width: 40px;
-    text-align: center;
-  }
-  .brand {
-    text-align: left !important;
-    font-size: 22px;
-    padding-left: 20px;
-    line-height: 50px !important;
-  }
-}
-@media (min-width: 767px) {
-  .nav-side-menu .menu-list .menu-content {
-    display: block;
-  }
 }
 body {
   margin: 0px;
   padding: 0px;
 }
-
-.nav-side-menu ul .sub-menu ul .sub-line li.active,
-.nav-side-menu li .sub-menu li .sub-line li.active {
-  color: #d19b3d;
-}
-
-.nav-side-menu ul .sub-menu li .sub-line li.active a,
-.nav-side-menu li .sub-menu li .sub-line li.active a {
-  color: #d19b3d;
-}
-.nav-side-menu ul .sub-menu li .sub-line li,
-.nav-side-menu li .sub-menu li .sub-line li {
-  background-color: #181c20;
-  border: none;
-  line-height: 28px;
-  border-bottom: 1px solid #23282e;
-  margin-left: 0px;
-}
-.nav-side-menu ul .sub-menu li .sub-line li:hover,
-.nav-side-menu li .sub-menu li .sub-line li:hover {
-  background-color: #020203;
-}
-.nav-side-menu ul .sub-menu li .sub-line li:before,
-.nav-side-menu li .sub-menu li .sub-line li:before {
-  font-family: FontAwesome;
-  content: "\f105";
-  display: inline-block;
-  padding-left: 100px;
-  padding-right: 10px;
-  vertical-align: middle;
-}
-
-.nav-side-menu .sub-menu li {
-  padding-left: 20px;
-  border-left: 3px solid #2e353d;
-  border-bottom: 1px solid #23282e;
-}
-.nav-side-menu .sub-menu li a {
-  text-decoration: none;
-  color: #e1ffff;
-}
-.sub-menu li a i {
-  padding-left: 10px;
-  width: 20px;
-  padding-right: 20px;
-}
-.nav-side-menu li .sub-menu li:hover {
-  border-left: 3px solid #d19b3d;
-  background-color: #4f5b69;
-  -webkit-transition: all 1s ease;
-  -moz-transition: all 1s ease;
-  -o-transition: all 1s ease;
-  -ms-transition: all 1s ease;
-  transition: all 1s ease;
-}
 @media (max-width: 767px) {
-  .nav-side-menu .sub-menu {
+  .nav-side-menu{
     position: relative;
     width: 100%;
     margin-bottom: 10px;
   }
-  .nav-side-menu .sub-menu .toggle-btn {
+  .nav-side-menu  {
     display: block;
     cursor: pointer;
     position: absolute;
@@ -248,79 +120,5 @@ body {
     text-align: center;
   }
 
-  .sub-line ul .sub-press li.active,
-  .sub-line li .sub-press li.active {
-    color: #d19b3d;
-  }
-
-  .sub-line ul .sub-press li.active a,
-  .sub-line li .sub-press li.active a {
-    color: #d19b3d;
-  }
-  .sub-line ul .sub-press li,
-  .sub-line li .sub-press li {
-    background-color: #181c20;
-    border: none;
-    line-height: 28px;
-    border-bottom: 1px solid #23282e;
-    margin-left: 0px;
-  }
-  .sub-line ul .sub-press li:hover,
-  .sub-line li .sub-press li:hover {
-    background-color: #020203;
-  }
-  .sub-line ul .sub-press li:before,
-  .sub-line li .sub-press li:before {
-    font-family: Arial;
-    content: "\f105";
-    display: inline-block;
-    padding-left: 50px;
-    padding-right: 10px;
-    vertical-align: middle;
-  }
-
-  .sub-line li {
-    padding-left: 20px;
-    border-left: 3px solid #2e353d;
-    border-bottom: 1px solid #23282e;
-  }
-  .sub-line li a {
-    text-decoration: none;
-    color: #e1ffff;
-  }
-  .sub-line li a i {
-    padding-left: 50px;
-    width: 20px;
-    padding-right: 20px;
-  }
-  .sub-line li:hover {
-    border-left: 3px solid #d19b3d;
-    background-color: #4f5b69;
-    -webkit-transition: all 1s ease;
-    -moz-transition: all 1s ease;
-    -o-transition: all 1s ease;
-    -ms-transition: all 1s ease;
-    transition: all 1s ease;
-  }
-  @media (max-width: 767px) {
-    .sub-line {
-      position: relative;
-      width: 100%;
-      margin-bottom: 10px;
-    }
-    .sub-line .toggle-btn {
-      display: block;
-      cursor: pointer;
-      position: absolute;
-      right: 10px;
-      top: 10px;
-      z-index: 10 !important;
-      padding: 3px;
-      background-color: #ffffff;
-      color: #000;
-      width: 40px;
-      text-align: center;
-    }
-  }
 }
 </style>
