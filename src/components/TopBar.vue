9c41fd9fd3ad4da40ba3f3a94d8bc001068e4882
GBstallman <nduwimanaguymich@gmail.com>
2021-03-11 14:21:02 +0200

    remove sccs files

---
+++ src/components/TopBar.vue
@@ -0,0 +1,78 @@
+
+<template>
+<!-- Navbar-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid justify-content-between">
+    <!-- Left elements -->
+    <div class="d-flex">
+      <!-- Brand -->
+<div class="brand"><h1> Pharmacy</h1></div>
+    </div>
+    <!-- Left elements -->
+
+  
+    <!-- Right elements -->
+    <ul class="navbar-nav flex-row">
+      <li class="nav-item me-3 me-lg-1">
+        <a class="nav-link d-sm-flex align-items-sm-center" href="#">
+          <img
+            src="https://mdbootstrap.com/img/new/avatars/1.jpg"
+            class="rounded-circle"
+            height="22"
+            alt=""
+            loading="lazy"
+          />
+          <strong class="d-none d-sm-block ms-1">GBstallman</strong>
+        </a>
+      </li>
+      <li class="nav-item dropdown me-3 me-lg-1">
+        <a
+          class="nav-link dropdown-toggle hidden-arrow"
+          href="#"
+          id="navbarDropdownMenuLink"
+          role="button"
+          data-mdb-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <i class="fas fa-power-off fa-lg"></i>
+        </a>
+        <ul
+          class="dropdown-menu dropdown-menu-end"
+          aria-labelledby="navbarDropdownMenuLink"
+        >
+          <li><a class="dropdown-item" href="#">Logout</a></li>
+        </ul>
+      </li>
+    </ul>
+    <!-- Right elements -->
+  </div>
+</nav>
+<!-- Navbar -->
+</template>
+
+<script>
+export default {
+  name: 'TopBar',
+  created() {
+
+  },
+  data() {
+    return {
+    }
+  },
+  props: {
+   
+  },
+  methods: {
+   
+  },
+};
+</script>
+
+<style scoped>
+.router :hover {
+   text-decoration: none;
+  list-style: none;
+}
+</style>
+
