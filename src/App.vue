1f22f992adb61e74176c35cfe370688edefcfa26
Desos <desnduwim26@gmail.com>
2021-03-11 17:24:03 +0200

    resolved

--- src/App.vue
+++ src/App.vue
@@ -9,33 +9,17 @@
    <div v-else>
       <Login @connected="performLogin"/>
   </div>
+  <div id="app">
+    <router-view/>
   </div>
 </template>
 
 <script>
 import SideBar from './components/SideBar'
-import TopBar from './components/TopBar'
-import Login from './components/Login'
-  export default  {
-    components: { SideBar, TopBar, Login},
-    data(){
-      return {
-        logged_in:false
-      }
-    },
-    methods:{
-      performLogin(user){
-        console.log(user)
-        this.logged_in=true;
-      },
-      logout(){
-        if (confirm("Vous etes sur de vouloir vous deconnecter?")) {
-          this.logged_in=true
-        }
-      }
-    }
-  };
 </script>
 <style scoped="">
 	
+
+<style>
+
 </style>
